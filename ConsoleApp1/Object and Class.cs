06a018c501956432c9449e872faa6b51a3072bb0
zmathantala <zmathantala@e4.co.za>
2024-06-07 02:15:15 +0200

    first commit

---
+++ ConsoleApp1/Object and Class.cs
@@ -0,0 +1,28 @@
+﻿using System;
+public class Student
+{
+    public int id;
+    public String name;
+    public void insert(int i, String n)
+    {
+        id = i;
+        name = n;
+    }
+    public void display()
+    {
+        Console.WriteLine(id + " " + name);
+    }
+}
+class TestStudent
+{
+    public static void Main(string[] args)
+    {
+        Student s1 = new Student();
+        Student s2 = new Student();
+        s1.insert(101, "Ajeet");
+        s2.insert(102, "Tom");
+        s1.display();
+        s2.display();
+
+    }
+}
---
+++ callfrom/call.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class call
+{
+    // User defined function  
+    public void Show(int val)
+    {
+        val *= val; // Manipulating value  
+        Console.WriteLine("Value inside the show function " + val);
+        // No return statement  
+    }
+}
